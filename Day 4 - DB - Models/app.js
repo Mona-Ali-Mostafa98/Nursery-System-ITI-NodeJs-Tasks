28c51f52fa3ab9909143facfeb0041258aca4752
Mona-Ali-Mostafa98 <monaali980628@gmail.com>
2024-03-22 10:35:59 +0200

    connect on db

--- Day 4 - DB - Models/app.js
+++ Day 4 - DB - Models/app.js
@@ -1,36 +1,46 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const teacherRoute = require("./Routes/teacherRoute");
 const childRoute = require("./Routes/childRoute");
 const classRoute = require("./Routes/classRoute");
 // 1- default function create server
 const server = express();
 
-//2- setting port number
+// 2- setting port number and Connect on db
 const port = process.env.PORT || 8080;
-server.listen(port, () => {
-  console.log("I am listening..........", port);
-});
+
+mongoose
+    .connect("mongodb://127.0.0.1:27017/NurserySystem")
+    .then(() => {
+        console.log("DB Connected Successfully....");
+        server.listen(port, () => {
+            console.log("I am listening..........", port);
+        });
+    })
+    .catch((error) => {
+        console.log("DB Problem ..." + error);
+    });
 
 /********************* Structure*************** */
 // first MW
 server.use((request, response, next) => {
-  console.log(request.url, request.method);
-  next();
+    console.log(request.url, request.method);
+    next();
 });
 
 /******************* EndPoints (Routes) */
 server.use(express.json());
 // server.use(express.urlencoded());
-// server.use(childRoute, teacherRoute, classRoute);
 server.use(teacherRoute, childRoute, classRoute);
 
 /******************************************/
 // Not Found
 server.use((request, response) => {
-  response.status(404).json({ data: "Not Found" });
+    response.status(404).json({data: "Not Found"});
 });
 
 // Error MW
 server.use((error, request, response, next) => {
-  response.status(500).json({ data: `Error MW ${error}` });
+    response.status(500).json({data: `Error MW ${error}`});
 });
