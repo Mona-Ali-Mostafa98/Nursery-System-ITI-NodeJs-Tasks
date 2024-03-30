9ced2c6db89b62f5df27633cf8a9333064c52085
Mona-Ali-Mostafa98 <monaali980628@gmail.com>
2024-03-30 03:54:15 +0200

    login controller after encrypte password

--- Project Complete - add multer - bcrypt - swagger packages/Controller/authController.js
+++ Project Complete - add multer - bcrypt - swagger packages/Controller/authController.js
@@ -2,7 +2,49 @@ const TeacherSchema = require("./../Model/TeacherModel");
 const ChildSchema = require("./../Model/ChildModel");
 
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
+exports.login = async (req, res, next) => {
+    try {
+        const {email, password} = req.body;
+
+        let user = await TeacherSchema.findOne({ email: email });
+
+        if (!user) {
+            user = await ChildSchema.findOne({ email: email });
+            if (!user) {
+                return res.status(401).json({ message: "Not Authenticated, Incorrect email or password......." });
+            }
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordCorrect) {
+            return res.status(401).json({ message: "Not Authenticated, Incorrect email or password......." });
+        }
+
+        role = user.role ? user.role : 'student';
+
+        const token = generateToken(user._id, role);
+        res.json({ message: "Login successfully.......", token: token });
+        
+    } catch (error) {
+        next(error);
+    }
+};
+
+function generateToken(userId, role) {
+    return jwt.sign(
+        {
+            _id: userId,
+            role: role,
+        },
+        process.env.SECRET_KEY || "Nursery System App",
+        {expiresIn: "24hr"}
+    );
+}
+
+/* // old code of lab
 exports.login = (req, res, next) => {
     TeacherSchema.findOne({
         email: req.body.email,
@@ -32,14 +74,4 @@ exports.login = (req, res, next) => {
         })
         .catch((error) => next(error));
 };
-
-function generateToken(userId, role) {
-    return jwt.sign(
-        {
-            _id: userId,
-            role: role,
-        },
-        process.env.SECRET_KEY || "Nursery System App",
-        {expiresIn: "24hr"}
-    );
-}
+*/
